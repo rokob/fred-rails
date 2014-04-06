2bf3d1326fd40a26da520c12d770aa6fa292fd33
Andrew Ledvina <led@fb.com>
2014-04-05 22:47:22 -0700

    created signup page and finished static page specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Fred::Application.routes.draw do
+  get "users/new"
   root "static_pages#home"
   match '/about',    to: "static_pages#about", via: 'get'
   match '/contact',  to: "static_pages#contact", via: 'get'
+
+  match '/signup',   to: "users#new", via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  let (:base_title) { "Fred" }
-
   subject { page }
 
   describe "Home page" do
     before(:each) { visit root_path }
+
     it { should have_content('Fred') }
     it { should have_title(full_title("")) }
     it { should_not have_title('| Home') }
@@ -15,12 +14,14 @@ describe "Static Pages" do
 
   describe "About page" do
     before(:each) { visit about_path }
+
     it { should have_content('About Us') }
     it { should have_title(full_title("About Us")) }
   end
 
   describe "Contact page" do
     before(:each) { visit contact_path }
+
     it { should have_content('Contact') }
     it { should have_title(full_title("Contact")) }
   end
@@ -31,9 +32,9 @@ describe "Static Pages" do
     expect(page).to have_title(full_title('About Us'))
     click_link "Contact"
     expect(page).to have_title(full_title('Contact'))
-    # click_link "Home"
-    # click_link "Sign up now!"
-    # expect(page).to # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
     click_link "fred"
     expect(page).to have_title(full_title(''))
   end
