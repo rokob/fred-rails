54cc7a8c50906baea20ffabbb1ce0726cd18184e
Andrew Ledvina <led@fb.com>
2014-04-05 22:38:11 -0700

    refactored tests a bit and added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,37 @@ describe "Static Pages" do
 
   let (:base_title) { "Fred" }
 
-  describe "Home page" do
-    it "should have the content 'Fred'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Fred')
-    end
+  subject { page }
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
+  describe "Home page" do
+    before(:each) { visit root_path }
+    it { should have_content('Fred') }
+    it { should have_title(full_title("")) }
+    it { should_not have_title('| Home') }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
+    before(:each) { visit about_path }
+    it { should have_content('About Us') }
+    it { should have_title(full_title("About Us")) }
+  end
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About")
-    end
+  describe "Contact page" do
+    before(:each) { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title("Contact")) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    find('footer').click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    # click_link "Home"
+    # click_link "Sign up now!"
+    # expect(page).to # fill in
+    click_link "fred"
+    expect(page).to have_title(full_title(''))
   end
 end
